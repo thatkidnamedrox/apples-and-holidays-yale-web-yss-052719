47a31e49a656bd0f4e41cdf5bd61415090886d19
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-05-22 20:05:01 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -85,12 +85,13 @@ def all_holidays_with_bbq(holiday_hash)
   # include the string "BBQ"
 
   holiday_names = []
-
+  
   holiday_hash.each do |season, holiday|
     holiday.each do |holiday_name, supplies|
 
       if supplies.include?("BBQ")
         holiday_names << holiday_name.intern
+        puts holiday_name.intern
       end
     end
     # if holiday_hash[season][holiday].include?("BBQ")
