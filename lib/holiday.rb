73d376faf13cb49fe81b51fcada28b7fb3331c76
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-05-22 19:02:04 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -49,7 +49,7 @@ end
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
   holiday_supplies[:winter].each do |holiday, supplies|
-    puts supplies
+    puts supplies.inspect
   end
 end
 
