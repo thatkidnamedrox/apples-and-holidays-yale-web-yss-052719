85fdf598e396a9e3f04b837373cfcc9057c78d6e
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-05-22 19:23:26 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -67,6 +67,7 @@ def all_supplies_in_holidays(holiday_hash)
     #season = key.to_s.split(" ").collect { |w| w.capitalize }
     #puts season
     season_array = key.to_s.split(" ").collect { |w| w.capitalize }
+    season = season_array.join(" ")
     puts season_array.inspect
   end
 end
