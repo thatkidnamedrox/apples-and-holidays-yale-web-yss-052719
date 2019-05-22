922840b957fb9da3ff12c03ef832e70bc00555c8
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-05-22 18:45:04 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -41,7 +41,7 @@ end
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   # code here
   supply_array.each do |supply|
-    holiday_hash[season][holiday_name] << supply
+    holiday_hash[season][holiday_name].push(supply)
   end
   # remember to return the updated hash
   holiday_hash
