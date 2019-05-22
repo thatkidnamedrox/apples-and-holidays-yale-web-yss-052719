72dfde874dfec3f4bea5479393a82150e07266cd
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-05-22 19:19:38 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -64,8 +64,9 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   holiday_hash.each do |key, value|
-    season = key.to_s.split(" ").collect { |w| w.capitalize }
-    puts season
+    #season = key.to_s.split(" ").collect { |w| w.capitalize }
+    #puts season
+    puts key
   end
 end
 
